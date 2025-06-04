37fdc84275f320686f12b0f49f67efdc092792c5
gangashivani7093 <darmavarapugangashivani@gmail.com>
2025-06-04 16:00:28 +0530

    Add files via upload

---
+++ UPPERTOLOWER.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class UPPERTOLOWER
+{
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    
+	    String a=sc.nextLine();
+	        System.out.println(atoLowerCase());
+		System.out.println(a.toUpperCase());
+		
+	}
+}
