bf186ff39853b44b0d5683e58bc37ba8d33ad1ad
gangashivani7093 <darmavarapugangashivani@gmail.com>
2025-06-04 16:00:11 +0530

    Add files via upload

---
+++ sum.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class sum{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner (System.in);
+        int []a=new int[5];
+        for(int i=0;i<a.length;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+		int sum=0;
+        for(int i=0;i<a.length;i++)
+        {
+            sum=sum+i
+            }
+              System.out.println(sum);
+		}
+		}
+        
