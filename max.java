456598453555e00b367cf6c3c3a31bb1fe875c13
gangashivani7093 <darmavarapugangashivani@gmail.com>
2025-06-04 15:57:22 +0530

    Add files via upload

---
+++ max.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class max{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner (System.in);
+        int []a=new int[5];
+        for(int i=0;i<a.length;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+		int main=0;
+        for(int i=0;i<a.length;i++)
+        {
+          if(a[i]>m)
+		  {
+		  m=a[i];
+            }
+		}
+		System.out.println(m);
+		}
+      }
