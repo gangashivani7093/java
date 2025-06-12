25b6f67cffd4de9b64680416042c907ccc201daf
gangashivani7093 <darmavarapugangashivani@gmail.com>
2025-06-12 15:47:27 +0530

    Add files via upload

---
+++ tabulation.java
@@ -0,0 +1,16 @@
+public class Main 
+{
+    public static void main(String[] args){
+        int n=3;
+        int[] memo=new int[n+1];
+        memo[1]=1;
+    
+      for(int i=2;i<memo.length;i++)
+        {
+            memo[i]=memo[i-1]+memo[i-2];
+            }
+           System.out.println(memo[n]);
+                    
+        
+        }
+    }
